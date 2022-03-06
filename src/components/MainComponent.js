708a0d7e1de145956a001e5765b9779899544d1f
Borhan Uddin <borhaninfos@gmail.com>
2022-03-07 01:23:09 +0600

    Update MainComponent.js

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,10 @@
 import React from "react";
+import Header from "./pages/header/Header";
 
 export default function MainComponent() {
-  return <div>MainComponent</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
