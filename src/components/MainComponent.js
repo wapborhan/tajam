06aeaf50279d4e8e9dbebd88d3201d7f6e834fde
Borhan Uddin <borhaninfos@gmail.com>
2022-03-07 01:34:21 +0600

    Update MainComponent.js

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,12 +1,26 @@
 import React from "react";
 import Header from "./pages/header/Header";
 import Story from "./pages/story/Story";
+import Watch from "./pages/watch/Watch";
+import Services from "./pages/services/Services";
+import Team from "./pages/team/Teams";
+// import Work from "./pages/work/Work";
+// import Clients from "./pages/clients/Clients";
+import Partner from "./pages/partner/Partner";
+import Footer from "./pages/footer/Footer";
 
 export default function MainComponent() {
   return (
     <div>
       <Header />
       <Story />
+      <Watch />
+      <Services />
+      <Team />
+      {/* <Work /> */}
+      {/* <Clients /> */}
+      <Partner />
+      <Footer />
     </div>
   );
 }
